b2e4a4caeeb7ed6fab8b49f215b9934de4a9923c
Ruochen <jayxu.uiuc@gmail.com>
2021-02-10 14:51:13 -0800

    Update install.sh
    fix

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 sudo apt -y update
 sudo apt -y install zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 git clone https://github.com/ruochenxu92/setup_zsh
 cp setup_zsh/example_zshrc ~/.zshrc
 echo 'exec zsh' >>  ~/.bashrc
 mkdir -p ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 source  ~/.zshrc
