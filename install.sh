df740dbf940fac7c716cef2a629405d7b61809ef
Ruochen <jayxu.uiuc@gmail.com>
2021-02-10 14:56:23 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ cp setup_zsh/example_zshrc ~/.zshrc
 echo 'exec zsh' >>  ~/.bashrc
 mkdir -p ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 source  ~/.zshrc
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -) -y"
